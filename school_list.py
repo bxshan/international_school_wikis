9d53f87fdb16028ebfe95dbf87a2171b8bc83093
Boxuan Shan <boxuan.shan@gmail.com>
2022-05-21 10:23:59 +0800

    Changed school according to the top 20 list from https://www.linstitute.net/hrbx2021

--- school_list.py
+++ school_list.py
@@ -1,4 +1,4 @@
-# Keystone Academy Beijing has no wikipedia page
+# Keystone Academy Beijing, BASIS International School Shenzhen, Dulwich College Shanghai Puxi, ISA International School of Guangzhou
 
 def get_school_list():
     school_list = [
@@ -7,38 +7,55 @@ def get_school_list():
         ["International School of Beijing", "Beijing", "China", "ISB"],
         ["Western Academy of Beijing", "Beijing", "China", "WAB"],
         ["Dulwich College Beijing", "Beijing", "China", "DCB"],
-        ["British School of Beijing", "Beijing", "China", "BSB"],
-        ["Beijing International Bilingual Academy", "Beijing", "China", "BIBA"],
-        ["Beijing City International School", "Beijing", "China", "BCIS"],
+        # ["British School of Beijing", "Beijing", "China", "BSB"],
+        # ["Beijing International Bilingual Academy", "Beijing", "China", "BIBA"],
+        # ["Beijing City International School", "Beijing", "China", "BCIS"],
         ["Harrow International School Beijing", "Beijing", "China", "HARROW"],
-        ["Tsinghua International School", "Beijing", "China", "TIS"],
+        # ["Tsinghua International School", "Beijing", "China", "TIS"],
         ["Canadian International School of Beijing", "Beijing", "China", "CISB"],
-        ["Springboard International Bilingual School", "Beijing", "China", "SIBS"],
+        # ["Springboard International Bilingual School", "Beijing", "China", "SIBS"],
+        ["Yew Chung International School of Beijing", "Beijing", "China", "YCIS BEIJING"],
 
          # SHANGHAI, CHINA
+         ["Shanghai High School International Division", "Shanghai", "China", "SISHD"],
+         ["Shanghai American School", "Shanghai", "China", "SAS"],
+         ["Concordia International School Shanghai", "Shanghai", "China", "CISS"],
          ["Western International School of Shanghai", "Shanghai", "China", "WISS"],
          ["Wellington College International Shanghai", "Shanghai", "China", "WCIS"],
-         ["Nord Anglia International School Shanghai Pudong", "Shanghai", "China", "NAIS PUDONG"],
-         ["Britannica International School Shanghai", "Shanghai", "China", "BISS"],
-         ["Dulwich College Shanghai", "Shanghai", "China", "DCS"],
-         ["YK Pao School", "Shanghai", "China", "YKPS"],
+         # ["Nord Anglia International School Shanghai Pudong", "Shanghai", "China", "NAIS PUDONG"],
+         # ["Britannica International School Shanghai", "Shanghai", "China", "BISS"],
+         ["Dulwich College Shanghai", "Shanghai", "China", "DCS PUDONG"],
+         # ["YK Pao School", "Shanghai", "China", "YKPS"],
+         ["Shanghai Singapore International School", "Shanghai", "China", "SSIS"],
+         ["Yew Chung International School of Shanghai", "Shanghai", "China", "YCIS SHANGHAI"],
 
          # SHENZHEN, CHINA
          ["Shekou International School", "Shenzhen", "China", "SIS"],
-         ["QSI International School of Shenzhen", "Shenzhen", "China", "QSI SHENZHEN"],
-         ["Merchiston International School", "Shenzhen", "China", "MIS"],
+         # ["QSI International School of Shenzhen", "Shenzhen", "China", "QSI SHENZHEN"],
+         # ["Merchiston International School", "Shenzhen", "China", "MIS"],
+         ["Shen Wai International School", "Shenzhen", "China", "SWIS"],
 
          # HONGKONG, CHINA
-         ["Stamford American School Hong Kong", "Hongkong", "China", "SASHK"],
-         ["Norwegian International School", "Hongkong", "China", "NIS"],
+         # ["Stamford American School Hong Kong", "Hongkong", "China", "SASHK"],
+         # ["Norwegian International School", "Hongkong", "China", "NIS"],
 
          # GUANGZHOU, China
          ["American International School of Guangzhou", "Guangzhou", "China", "AISG"],
-         ["Alcanta International College", "Guangzhou", "China", "AIC"],
-         ["International School of Dongguan", "Guangzhou", "China", "ISD"],
-         ["Utahloy International School Guangzhou", "Guangzhou", "China", "UISG"],
-         ["Utahloy International School Zengcheng", "Guangzhou", "China", "UISZ"],
+         # ["Alcanta International College", "Guangzhou", "China", "AIC"],
+         # ["International School of Dongguan", "Guangzhou", "China", "ISD"],
+         # ["Utahloy International School Guangzhou", "Guangzhou", "China", "UISG"],
+         # ["Utahloy International School Zengcheng", "Guangzhou", "China", "UISZ"],
          ["British School of Guangzhou", "Guangzhou", "China", "BSG"],
+
+         # TIANJIN, CHINA
+         ["Wellington College International Tianjin", "Tianjin", "China", "WCIT"],
+
+         # SUZHOU, CHINA
+         ["Dulwich College Suzhou", "Suzhou", "China", "DCSZ"],
+
+         # NANJING, CHINA
+         ["Nanjing International School", "Nanjing", "China", "NIS"],
+
     ]
 
     return school_list
